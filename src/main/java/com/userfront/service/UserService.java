cbdf4ead2e1f14047f64965015baec12e28fa3b2
Renata Srbljanin <renatasrbljanin@gmail.com>
2017-12-03 16:23:09 +0100

    edit Profile function added

--- src/main/java/com/userfront/service/UserService.java
+++ src/main/java/com/userfront/service/UserService.java
@@ -6,23 +6,21 @@ import com.userfront.domain.User;
 import com.userfront.domain.security.UserRole;
 
 public interface UserService {
-	
+
 	User findByUsername(String username);
-	
+
 	User findByEmail(String email);
-	
+
 	boolean checkUserExists(String username, String email);
-	
+
 	boolean checkUsernameExists(String username);
-	
+
 	boolean checkEmailExists(String email);
-	
+
 	void save(User user);
 
 	User createUser(User user, Set<UserRole> userRoles);
-	
-	User saveUser (User user);
-	
-	
+
+	User saveUser(User user);
 
 }
