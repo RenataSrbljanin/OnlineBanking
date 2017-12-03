b10e52bcf1908b05f0ae84b84ff12ad222a6ffd3
Renata Srbljanin <renatasrbljanin@gmail.com>
2017-12-03 11:01:23 +0100

    Transfer to someone else added

--- src/main/java/com/userfront/service/TransactionService.java
+++ src/main/java/com/userfront/service/TransactionService.java
@@ -1,27 +1,40 @@
 package com.userfront.service;
 
+import java.security.Principal;
 import java.util.List;
 
 import com.userfront.domain.PrimaryAccount;
 import com.userfront.domain.PrimaryTransaction;
+import com.userfront.domain.Recipient;
 import com.userfront.domain.SavingsAccount;
 import com.userfront.domain.SavingsTransaction;
 
 public interface TransactionService {
 
 	public List<PrimaryTransaction> findPrimaryTransactionList(String username);
-	
+
 	public List<SavingsTransaction> findSavingsTransactionList(String username);
-	
+
 	public void savePrimaryDepositTransaction(PrimaryTransaction primaryTransaction);
-	
+
 	public void saveSavingsDepositTransaction(SavingsTransaction savingsTransaction);
 
 	void savePrimaryWithdrawTransaction(PrimaryTransaction primaryTransaction);
-	
+
 	void saveSavingsWithdrawTransaction(SavingsTransaction savingsTransaction);
 
 	public void betweenAccountsTransfer(String transferFrom, String transferTo, String amount,
 			PrimaryAccount primaryAccount, SavingsAccount savingsAccount) throws Exception;
-	
+
+	public List<Recipient> findRecipientList(Principal principal);
+
+	public Recipient saveRecipient(Recipient recipient);
+
+	public Recipient findRecipientByName(String recipientName);
+
+	public void deleteRecipientByName(String recipientName);
+
+	public void toSomeoneElseTransfer(Recipient recipient, String accountType, String amount,
+			PrimaryAccount primaryAccount, SavingsAccount savingsAccount);
+
 }
